4e39762fa41391f4ae46933c960fad9a4c5ee1c2
Alejandro Yañez Meseguer <aymeseguer@gmail.com>
2026-01-16 23:20:18 +0100

    feat: fade out transition

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,12 +1,14 @@
 extends Control
 
 @onready var start_game: AudioStreamPlayer = $PlayWrapperContainer/StartGame
-
+@export var vignette_duration: float = 1.2
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
+		get_tree().root.set_input_as_handled()
+		play_vignette()
 		start_game.play()
-		await
+		await start_game.finished
 		get_tree().change_scene_to_file("res://Scenes/Game.tscn")
 		
 		
@@ -14,3 +16,11 @@ func _unhandled_input(event: InputEvent) -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ResourceLoader.load_threaded_request('res://Scenes/Game.tscn')
+	var color_rect = $CanvasLayer/ColorRect
+	color_rect.material.set_shader_parameter("progress", 1.0)
+
+
+func play_vignette() -> void:
+	var color_rect = $CanvasLayer/ColorRect
+	var tween = create_tween()
+	tween.tween_property(color_rect.material, "shader_parameter/progress", 0.0, vignette_duration)
